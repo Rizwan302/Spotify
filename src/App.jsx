7ed7aaa063a2ccf780d94b30b979bea04cc7cdd8
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-21 20:17:21 +0500

    Remove Heading

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
       <div>
         
       </div>
-      <h1>Vite + React</h1>
-      
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
   )
 }
 

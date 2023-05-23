185348d9d37b0f47b30878e934aa4c1397ac5383
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-24 00:41:02 +0500

    Add new file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import './Navbar.scss'
+import { AiFillHome } from 'react-icons/ai';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { BiLibrary } from 'react-icons/bi';
 
 export default function Navbar() {
   return (
@@ -8,6 +11,21 @@ export default function Navbar() {
         <div className="logobar">
           <img src="./public/logo.png" className='logo' alt="Spotify" />
           <span className='logotext'>Spotify</span>
+        </div>
+
+        <div className="icon">
+          <nav>
+            <ul>
+              <li><AiFillHome className='icons'/> <a href=""> Home</a></li>
+              <li><AiOutlineSearch className='icons'/><a href=""> Search</a></li>
+              <li><BiLibrary className='icons'/><a href=""> Your Library</a></li>
+            </ul>
+          </nav>
+          <div className="">
+            hello
+          {/* <li><AiFillHome className=''/> <a href=""> Create Playlist</a></li>
+          <li><AiFillHome className=''/> <a href=""> Liked Songs</a></li> */}
+          </div>
         </div>
 
       </div>

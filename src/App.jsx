10ec5d7e479b0e0c9dad3f4e3614a9343958b6b7
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-23 23:15:33 +0500

    Add new file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import './App.css'
+import './App.scss'
+import Navbar from './components/Navbar'
+import Loginbar from './components/Loginbar'
 
 function App() {
 
   return (
-      <div>
-        
+      <div className='App'>
+        <Navbar />
+        <Loginbar/>
       </div>
   )
 }
---
+++ src/components/Loginbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Loginbar.scss'
+
+export default function Loginbar() {
+  return (
+    <div className='loginbar'>
+      login
+    </div>
+  )
+}

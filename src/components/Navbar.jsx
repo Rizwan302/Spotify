9a6b8ef748d7b1b32b687449835805b6c5eefada
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-23 23:38:41 +0500

    Add new file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,13 @@ import './Navbar.scss'
 export default function Navbar() {
   return (
     <div className='navbar'>
-      hello
+      <div className="warpar">
+        <div className="logobar">
+          <img src="./public/logo.png" className='logo' alt="Spotify" />
+          <span className='logotext'>Spotify</span>
+        </div>
+
+      </div>
     </div>
   )
 }

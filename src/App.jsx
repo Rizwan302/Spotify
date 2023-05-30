77babaf194641ab14ada213073d9b32ded4fb79c
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-05-30 11:26:27 +0500

    Remove state

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { useState } from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar";
 import Loginbar from "./components/Secendbar";
 import Nav from "./components/Nav";
 
 function App() {
-  const [theme, setTheme] = useState({
-    background: "#0F0F0F",
-    color: "white",
-  });
 
-  // const UpdateMode = () => {
-  //   if (theme.color === "white") {
-  //     setTheme({
-  //       background: "#F5FFFA",
-  //       color: "#0F0F0F",
-  //     });
-  //   } else {
-  //     setTheme({
-  //       background: "#0F0F0F",
-  //       color: "white",
-  //     });
-  //   }
-  // };
-
+  
   return (
     <div className="App">
       {/* <Navbar /> */}

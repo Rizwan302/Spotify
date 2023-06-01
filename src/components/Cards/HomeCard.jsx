9b6421592a504191d70f5bf33afbdc0fcc54b014
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-06-01 17:18:22 +0500

    Add Img

--- src/components/Cards/HomeCard.jsx
+++ src/components/Cards/HomeCard.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
-import './HomeCard.scss'
+import React from "react";
+import "./HomeCard.scss";
 
-export default function HomeCard({title}) {
+import play from "./Img/play.png";
+import pause from "./Img/pause.png";
+
+export default function HomeCard({ title }) {
   return (
-    <div className='cards'>
-        <div className="card-body">
-        <img src="https://www.teachhub.com/wp-content/uploads/2019/10/Our-Top-10-Songs-About-School-768x569.png" alt="" />
-      <div className="text">
-        <h5>{title}</h5>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        </p>
-      </div>
+    <div className="cards">
+      <div className="card-body">
+        <img
+          src="https://www.teachhub.com/wp-content/uploads/2019/10/Our-Top-10-Songs-About-School-768x569.png"
+          alt=""
+        />
+        <div className="botton">
+          <img src={play} alt="" />
+          {/* <img src={pause} alt="" /> */}
+        </div>
+        {/* <img type='button' src={play} alt="" classNameName='play'/> */}
+
+        <div className="text">
+          <h5>{title}</h5>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }

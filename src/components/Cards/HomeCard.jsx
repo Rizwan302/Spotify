c4323b043b067ec7a988f2027197906cd5f12d1b
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-06-01 17:29:51 +0500

    Add state

--- src/components/Cards/HomeCard.jsx
+++ src/components/Cards/HomeCard.jsx
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./HomeCard.scss";
 
 import play from "./Img/play.png";
 import pause from "./Img/pause.png";
 
 export default function HomeCard({ title }) {
+  const [noMusic, setNoMusic] = useState(play)
+
+  const buttonset = () => {
+    if(noMusic === play){
+      setNoMusic(pause)
+      console.log('play')
+    }
+    else if(noMusic === pause){
+      setNoMusic(play)
+      console.log('pause')
+    }
+  }
+
   return (
     <div className="cards">
-      <div className="card-body">
+      <div className="card-body" onClick={buttonset}>
         <img
           src="https://www.teachhub.com/wp-content/uploads/2019/10/Our-Top-10-Songs-About-School-768x569.png"
           alt=""
         />
         <div className="botton">
-          <img src={play} alt="" />
-          {/* <img src={pause} alt="" /> */}
+          <img src={noMusic} alt=""  />
         </div>
         {/* <img type='button' src={play} alt="" classNameName='play'/> */}
 
